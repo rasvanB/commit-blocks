f2667a7035f548ffdb856c6b10bdbe5c97ab601c
rasvanB <not.obelix@gmail.com>
2023-08-07 23:55:24 +0300

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,27 @@
-import { z } from "zod";
+import ThreeCanvas from "@/components/canvas";
+import SearchUser from "@/components/search-user";
+import UserInfo from "@/components/user-info";
+import { fetchContributions } from "@/utils/fetch";
 
-const daysArraySchema = z.array(
-  z.object({
-    date: z.string().transform((date) => new Date(date)),
-    contributionCount: z.number(),
-    color: z.string(),
-  })
-);
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { username: string | undefined };
+}) {
+  if (!searchParams.username)
+    return (
+      <>
+        <SearchUser />
+      </>
+    );
+  const data = await fetchContributions(searchParams.username);
 
-async function fetchContributions(username: string) {
-  const NOW = new Date().toISOString().replace(/\.\d+Z/, "Z");
-  const ONE_YEAR_AGO = new Date(Date.now() - 1000 * 60 * 60 * 24 * 365)
-    .toISOString()
-    .replace(/\.\d+Z/, "Z");
+  if (!data) return <div className="text-white">User not found</div>;
 
-  const data = await fetch("https://api.github.com/graphql", {
-    method: "POST",
-    body: JSON.stringify({
-      query: `
-          {
-            user(login: "${username}") {
-              contributionsCollection(
-                from: "${ONE_YEAR_AGO}"
-                to: "${NOW}"
-              ) {
-                contributionCalendar {
-                  weeks {
-                    contributionDays {
-                      date
-                      contributionCount
-                      color
-                    }
-                  }
-                }
-              }
-            }
-          }
-        `,
-    }),
-    headers: {
-      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((res) => res.data);
-
-  const days = data.user.contributionsCollection.contributionCalendar.weeks
-    .map((w: Record<PropertyKey, any>) => w.contributionDays)
-    .flat();
-
-  const result = daysArraySchema.safeParse(days);
-
-  if (!result.success) return [];
-  return result.data;
-}
-
-export default async function Home() {
-  const days = await fetchContributions("rasvanB");
-
-  return <main></main>;
+  return (
+    <>
+      <UserInfo username={searchParams.username} avatarUrl={data.avatarUrl} />
+      <ThreeCanvas contributionDays={data.contributionDays} />
+    </>
+  );
 }
