1d9ef45d15024f3adca9a90988ce1aa1857623e4
rasvanB <not.obelix@gmail.com>
2023-08-08 22:53:34 +0300

    added bg image

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -85,7 +85,7 @@ const ThreeCanvas = ({ contributionDays }: { contributionDays: Day[] }) => {
       </div>
     );
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full bg-backgroundImage bg-no-repeat bg-cover backdrop-blur-3xl">
       <Canvas camera={{ position: [45, 40, 0] }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        backgroundImage: "url('/bg.png')",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
