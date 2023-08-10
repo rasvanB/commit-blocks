5d77e0a1cbfb594f873f9a8ab733506c985cccc3
rasvanB <not.obelix@gmail.com>
2023-08-10 17:26:10 +0300

    moved constants outside compontents

--- src/components/contribution-map.tsx
+++ src/components/contribution-map.tsx
@@ -2,29 +2,35 @@ import { Day } from "@/utils/fetch";
 import Box from "./box";
 
 const MAX_HEIGHT = 5;
+const ROWS = 7;
+const COLS = 365 / ROWS;
+const BASE_HEIGHT = 2;
 
 const ContributionMap = ({ contributionDays }: { contributionDays: Day[] }) => {
   const maxContributions = Math.max(
     ...contributionDays.map((day) => day.contributionCount)
   );
-  const ROWS = 7;
-  const COLS = 365 / ROWS;
 
   return (
     <mesh position={[-ROWS / 2, 0, -COLS / 2]}>
       {contributionDays.map((day, i) => {
-        const height = (day.contributionCount / maxContributions) * MAX_HEIGHT;
+        const HEIGHT = (day.contributionCount / maxContributions) * MAX_HEIGHT;
         return (
           <Box
             day={day}
             key={i}
-            size={[1, height, 1]}
-            position={[i % ROWS, height / 2 + 2.01, Math.floor(i / ROWS)]}
+            size={[1, HEIGHT, 1]}
+            // 0.01 is to avoid z-fighting
+            position={[
+              i % ROWS,
+              HEIGHT / 2 + BASE_HEIGHT + 0.01,
+              Math.floor(i / ROWS),
+            ]}
           />
         );
       })}
       <mesh position={[ROWS / 2 - 0.5, 1, COLS / 2]}>
-        <boxGeometry args={[ROWS + 2, 2, COLS + 2]} />
+        <boxGeometry args={[ROWS + 2, BASE_HEIGHT, COLS + 2]} />
         <meshStandardMaterial color={"gray"} />
       </mesh>
     </mesh>
