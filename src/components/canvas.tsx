3ea97fb118924156afe0f607a61c8571f76838fe
rasvanB <not.obelix@gmail.com>
2023-08-18 22:28:14 +0300

    fixed rotation

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -20,7 +20,7 @@ const ThreeCanvas = ({ contributionDays }: { contributionDays: Day[] }) => {
     );
   return (
     <div className="w-full h-full bg-backgroundImage bg-no-repeat bg-cover backdrop-blur-3xl">
-      <Canvas camera={{ position: [45, 40, 0] }}>
+      <Canvas camera={{ position: [-45, 40, 0] }}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[10, 10, 5]} />
         <ContributionMap contributionDays={contributionDays} />
